082aedd6074d37480b7a7edf00edca71f182166b
KitiHey <tomoiagalaurentiu2017@gmail.com>
2025-09-04 02:37:21 +0200

    Basic communication

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log/slog"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Initializing client...\n")
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error(".env couldn't be loaded!: %e", err)
+		return
+	}
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		slog.Error(".env doesn't have a 'PORT'!")
+		return
+	}
+	conn, err := net.Dial("tcp", "localhost"+port)
+	if err != nil {
+		slog.Error("Error connecting!: %e", err)
+		return
+	}
+	conn.Write([]byte("Hello from Client!"))
+	var buffer = make([]byte, 1024)
+	conn.Read(buffer)
+	fmt.Println(string(buffer))
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log/slog"
+	"net"
+	"os"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error(".env couldn't be loaded!: %e", err)
+		return
+	}
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		slog.Error(".env doesn't have a 'PORT'!")
+		return
+	}
+
 	fmt.Printf("Initializing server...\n")
+	ln, err := net.Listen("tcp", port)
+	if err != nil {
+		slog.Error("Error listening!: %e", err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			slog.Error("Connection failed!: %e", err)
+		}
+		conn.Write([]byte("Welcome to TCP Chat!"))
+		var buffer = make([]byte, 1024)
+		conn.Read(buffer)
+		fmt.Println(string(buffer))
+	}
 }
